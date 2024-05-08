1432d847da7cece4098e6bece06c80ba9e1416f0
uzair-sayyed-practice <133044775+uzair-sayyed-practice@users.noreply.github.com>
2024-05-08 18:47:15 +0530

    first commit

---
+++ section2/main.js
@@ -0,0 +1,32 @@
+function logUpto(n){
+    for(let i = 1; i <= n; i++){
+        console.log(i);
+    }
+}
+// let t1 = performance.now();
+// logUpto(10);
+// let t2 = performance.now();
+// console.log(t2 - t1);
+
+
+function logAtmost10(n){
+    for(let i = 1; i <= Math.min(n, 10); i++){
+        console.log(i);
+    }
+}
+// let t1 = performance.now();
+// logAtmost10(4);
+// let t2 = performance.now();
+// console.log(t2 - t1);
+// logAtmost10(100);
+
+
+function logAtleast10(n){
+    for(let i = 1; i <= Math.max(n, 10); i++){
+        // console.log(i);
+    }
+}
+// let t1 = performance.now();
+// logAtleast10(1000);
+// let t2 = performance.now();
+// console.log(t2 - t1);
